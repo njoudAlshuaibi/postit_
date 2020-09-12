0ab1ae0da0772883fa9ffa21262c826f681326a8
njoudAlshuaibi <70709650+njoudAlshuaibi@users.noreply.github.com>
2020-09-12 20:53:43 +0300

    Initial commit

---
+++ app/src/main/java/com/postit/postit_/MainActivity.java
@@ -0,0 +1,21 @@
+package com.postit.postit_;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("mehhhffssage");
+
+        myRef.setValue("Hello, World!ggggg");
+    }
+}
