5bec38d910d5bc89645d25584c74c787736fda22
njoudAlshuaibi <70709650+njoudAlshuaibi@users.noreply.github.com>
2020-09-14 09:42:51 +0300

    splash-create account

---
+++ app/src/main/java/com/postit/postit_/CreateAccountActivity.java
@@ -0,0 +1,17 @@
+package com.postit.postit_;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+
+
+    }
+}
--- app/src/main/java/com/postit/postit_/MainActivity.java
+++ app/src/main/java/com/postit/postit_/MainActivity.java
@@ -2,28 +2,37 @@ package com.postit.postit_;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private TextView createAccount;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("mehhhffssage");
+        DatabaseReference myRef = database.getReference("");
 
-        myRef.setValue("Hello, World!ggggg");//lklkklklkl
+        createAccount=findViewById(R.id.createAccount);
+        createAccount.setOnClickListener(new View.OnClickListener() {
 
-        DatabaseReference myf = database.getReference("mesho");
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CreateAccountActivity.class));
+            }//End onClick()
+        });
 
-        myf.setValue("jojo");//lklkklklkl
 
-        DatabaseReference myof = database.getReference("lujain");
+        }
 
-        myof.setValue("raya");//lklkklklkl
+
+
+
     }
-}
