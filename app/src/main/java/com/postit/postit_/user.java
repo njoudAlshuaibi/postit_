24a0e30b8f466c611095486069ca60c0b5eba36a
rayakhalid <reyakh.h@gmail.com>
2020-09-15 11:10:14 +0300

    registration // juju take this

---
+++ app/src/main/java/com/postit/postit_/user.java
@@ -0,0 +1,18 @@
+package com.postit.postit_;
+
+import android.widget.EditText;
+
+public class user {
+    public String username,email,password,college,major;
+
+    public user(){}
+
+    public user(String userName, String email, String signupPass,String scollege, String major){
+        this.username = userName;
+                this.email = email;
+                this.password = signupPass;
+                this.college = scollege;
+                this.major = major;
+    }
+
+}
