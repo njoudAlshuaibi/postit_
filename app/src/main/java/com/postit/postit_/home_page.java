12a8b42173adfe6049e6d220699f29ccb553c16b
njoudAlshuaibi <70709650+njoudAlshuaibi@users.noreply.github.com>
2020-09-16 01:05:31 +0300

    Logout frontend DONE

--- app/src/main/java/com/postit/postit_/home_page.java
+++ app/src/main/java/com/postit/postit_/home_page.java
@@ -1,8 +1,15 @@
 package com.postit.postit_;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class home_page extends AppCompatActivity {
 
@@ -10,5 +17,49 @@ public class home_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+Toolbar toolbar = findViewById(R.id.toolbar_homepage);
+setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id==R.id.exit){
+            AlertDialog.Builder builder = new AlertDialog.Builder(home_page.this);
+            builder.setMessage("Are you Sure you want to exit?");
+            builder.setCancelable(true);
+
+            builder.setNegativeButton("YES", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    //fireBase auth.signout
+                    finish();
+                    Intent intent = new Intent(home_page.this,MainActivity.class);
+                    startActivity(intent);
+
+                }
+            });
+
+            builder.setPositiveButton("NO", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+
+        }
+        return true;
     }
 }
