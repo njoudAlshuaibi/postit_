5cc8ad307f3e225a8b122bdc5230de51a785fe2e
njoudAlshuaibi <70709650+njoudAlshuaibi@users.noreply.github.com>
2020-09-16 01:12:51 +0300

    ريا تكفين شوفي

--- app/src/main/java/com/postit/postit_/MainActivity.java
+++ app/src/main/java/com/postit/postit_/MainActivity.java
@@ -17,7 +17,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-//raya
+//raya,,,
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView createAccount;//test
