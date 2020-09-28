7d4eff5fb91ffc424634002bb753729defd062c0
Mashael <Mashael.omarr@gmail.com>
2020-09-29 00:35:16 +0300

    add major

--- app/src/main/java/com/postit/postit_/AdminActivity.java
+++ app/src/main/java/com/postit/postit_/AdminActivity.java
@@ -18,10 +18,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class AdminActivity extends AppCompatActivity  {
 
-    Spinner spinner3;
-    private String adminmajor;
 
-// mesho l love you <3
 
 
 
@@ -36,22 +33,7 @@ public class AdminActivity extends AppCompatActivity  {
 
 
 
-        spinner3 = findViewById(R.id.spinner3);
-        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this, R.array.AdminMajor,android.R.layout.simple_spinner_item);
-        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner3.setAdapter(adapter3);
-        spinner3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                adminmajor = parent.getItemAtPosition(position).toString();
 
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
 
     }
 
