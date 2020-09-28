aed3f17049824202d3a359b2b3021ba956c1f191
Mashael <Mashael.omarr@gmail.com>
2020-09-28 03:11:50 +0300

    frontend pages

---
+++ app/src/main/java/com/postit/postit_/Visitors_activity.java
@@ -0,0 +1,14 @@
+package com.postit.postit_;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Visitors_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visitors_activity);
+    }
+}
