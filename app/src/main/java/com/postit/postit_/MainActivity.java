12ee0eae721e768e73d162d6d69c1e4d680e7843
rayakhalid <reyakh.h@gmail.com>
2020-10-25 07:08:56 +0300

    1

--- app/src/main/java/com/postit/postit_/MainActivity.java
+++ app/src/main/java/com/postit/postit_/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText editTextEmail;
     private EditText editTextPassword;
     //juju end
-
+//n
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
