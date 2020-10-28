ab69701b66ee14b649d91e5855a8e5021e0f5097
Lujain Z <55849375+Lujain993@users.noreply.github.com>
2020-10-28 05:11:42 +0300

    تعديلات مب بسيطه مثل الي قبلها

---
+++ app/src/main/java/com/postit/postit_/Student_Visitor/chatActivity.java
@@ -0,0 +1,16 @@
+package com.postit.postit_.Student_Visitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.postit.postit_.R;
+
+public class chatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
