b6c511e59a9069f7b1bd10b7706d6492e2a1a488
AljawharaAlhamlan <lo0lo0-131@hotmail.com>
2020-11-04 00:38:01 +0300

    chat "clean"

--- app/src/main/java/com/postit/postit_/Student_Visitor/chatActivity.java
+++ app/src/main/java/com/postit/postit_/Student_Visitor/chatActivity.java
@@ -54,7 +54,7 @@ public class chatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        // massagesRef = FirebaseDatabase.getInstance().getReference().child("Massages");
+
         setListener();
         usersRef = FirebaseDatabase.getInstance().getReference().child("users");
 
@@ -104,16 +104,6 @@ public class chatActivity extends AppCompatActivity {
                 }
                 edtPost.setText("");
 
-//                if (edtPost.getText() != null && !edtPost.getText().toString().equals("")&& !edtPost.getText().toString().trim().equals("")){
-//
-//                    String key = massagesRef.push().getKey();
-//                    Date date = new Date();
-//                    massagesRef.child(key).setValue(new chat(key, edtPost.getText().toString(), FirebaseAuth.getInstance().getUid(), sharedPreference.getString(getApplicationContext(), Constants.keys.CLICKED_PARENT,"error"), date.getTime()));
-//                    // Log.d("CHATACT", "after creating messege" );
-//                    edtPost.setText("");
-//
-//                }
-
 
             }//onClick
         });
@@ -129,70 +119,7 @@ public class chatActivity extends AppCompatActivity {
 
     private void setListener() {
 
-//        massagesRef.addChildEventListener(new ChildEventListener() {
-//            @Override
-//            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-//
-//                chat chat = dataSnapshot.getValue(chat.class);
-//
-//                if(chat.getReceiverId().equals( sharedPreference.getString(getApplicationContext(), Constants.keys.CLICKED_PARENT,"error"))
-//                        && chat.getSenderId().equals(FirebaseAuth.getInstance().getUid()))
-//                    chatList.add(chat);
-//                else if(chat.getSenderId().equals(sharedPreference.getString(getApplicationContext(), Constants.keys.CLICKED_PARENT,"error"))
-//                        && chat.getReceiverId().equals(FirebaseAuth.getInstance().getUid()))
-//                    chatList.add(chat);
-//
-//                if (adapter != null) {
-//                    adapter.notifyDataSetChanged();
-//                    recyclerView.scrollToPosition(chatList.size()-1);
-//                }
-//
-//            }
-//
-//            @Override
-//            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-//
-//                Log.i(TAG, "onChildChanged");
-//                chat chat = dataSnapshot.getValue(chat.class);
-//
-//                for (int i = 0; i < chatList.size(); i++) {
-//                    if (chatList.get(i).getId().equals(chat.getId())) {
-//                        chatList.set(i, chat);
-//                        adapter.notifyDataSetChanged();
-//                        break;
-//                    }
-//                }
-//
-//            }
-//
-//            @Override
-//            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-//                Log.i("onChildRemoved", "onChildRemoved");
-//
-//                chat chat = dataSnapshot.getValue(chat.class);
-//
-//                for (int i = 0; i < chatList.size(); i++) {
-//                    if (chatList.get(i).getId().equals(chat.getId())) {
-//                        chatList.remove(i);
-//                        adapter.notifyDataSetChanged();
-//                        break;
-//                    }
-//                }
-//
-//            }
-//
-//            @Override
-//            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-//                Log.i("onChildMoved", "onChildMoved");
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//                Log.i("onCancelled", "onCancelled");
-//
-//            }
-//        });
+
     }//end setListener
 
     @Override
