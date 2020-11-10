910e4ee6fb37851968a0e5e024265ac6edc4199f
njoudAlshuaibi <70709650+njoudAlshuaibi@users.noreply.github.com>
2020-11-11 00:10:14 +0300

    chatting notification 2

--- app/src/main/java/com/postit/postit_/Student_Visitor/chatActivity.java
+++ app/src/main/java/com/postit/postit_/Student_Visitor/chatActivity.java
@@ -189,18 +189,18 @@ public class chatActivity extends AppCompatActivity {
         });
 
 
-////Notification
-//        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
-//
-//        // OneSignal Initialization
-//        OneSignal.startInit(this)
-//                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
-//                .unsubscribeWhenNotificationsAreDisabled(true)
-//                .init();
-//
-//        String LoggedIn_User_Email =FirebaseAuth.getInstance().getCurrentUser().getEmail();
-//        OneSignal.sendTag("User_ID",LoggedIn_User_Email);
-//        //End notify
+//Notification
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+
+        // OneSignal Initialization
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+
+        String LoggedIn_User_Email =FirebaseAuth.getInstance().getCurrentUser().getEmail();
+        OneSignal.sendTag("User_ID",LoggedIn_User_Email);
+        //End notify
 
 
 
@@ -265,80 +265,80 @@ public class chatActivity extends AppCompatActivity {
         return true;
     }
 
-//    private void sendNotification(final String message, final String senderMail)
-//    {
-//        AsyncTask.execute(new Runnable() {
-//            @Override
-//            public void run() {
-//                int SDK_INT = android.os.Build.VERSION.SDK_INT;
-//                if (SDK_INT > 8) {
-//                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
-//                            .permitAll().build();
-//                    StrictMode.setThreadPolicy(policy);
-//                    String send_email;
-//
-//                    //This is a Simple Logic to Send Notification different Device Programmatically....
-//                    String LoggedIn_User_Email =FirebaseAuth.getInstance().getCurrentUser().getEmail();
-//                    OneSignal.sendTag("User_ID",FirebaseAuth.getInstance().getCurrentUser().getEmail());//sender email
-//                    send_email=senderMail;//reciever email
-//                    /*
-//                    if (MainActivity.LoggedIn_User_Email.equals("user1@gmail.com")) {
-//                        send_email = "user2@gmail.com";
-//                    } else {
-//                        send_email = "user1@gmail.com";
-//                    }*/
-//
-//                    try {
-//                        String jsonResponse;
-//
-//                        URL url = new URL("https://onesignal.com/api/v1/notifications");
-//                        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-//                        con.setUseCaches(false);
-//                        con.setDoOutput(true);
-//                        con.setDoInput(true);
-//
-//                        con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-//                        con.setRequestProperty("Authorization", "Basic NTVjOWIxNmYtYjI0YS00NjU0LWE1YmEtYjM5YTM2OWQxZjIx");
-//                        con.setRequestMethod("POST");
-//
-//                        String strJsonBody = "{"
-//                                + "\"app_id\": \"c12cdbbf-7bd8-4c4f-b5ba-df37e6cc2d36\","
-//
-//                                + "\"filters\": [{\"field\": \"tag\", \"key\": \"User_ID\", \"relation\": \"=\", \"value\": \"" + send_email + "\"}],"
-//
-//                                + "\"data\": {\"foo\": \"bar\"},"
-//                                + "\"contents\": {\"en\": \""+message+"\"}"
-//                                + "}";
-//
-//
-//                        System.out.println("strJsonBody:\n" + strJsonBody);
-//
-//                        byte[] sendBytes = strJsonBody.getBytes("UTF-8");
-//                        con.setFixedLengthStreamingMode(sendBytes.length);
-//
-//                        OutputStream outputStream = con.getOutputStream();
-//                        outputStream.write(sendBytes);
-//
-//                        int httpResponse = con.getResponseCode();
-//                        System.out.println("httpResponse: " + httpResponse);
-//
-//                        if (httpResponse >= HttpURLConnection.HTTP_OK
-//                                && httpResponse < HttpURLConnection.HTTP_BAD_REQUEST) {
-//                            Scanner scanner = new Scanner(con.getInputStream(), "UTF-8");
-//                            jsonResponse = scanner.useDelimiter("\\A").hasNext() ? scanner.next() : "";
-//                            scanner.close();
-//                        } else {
-//                            Scanner scanner = new Scanner(con.getErrorStream(), "UTF-8");
-//                            jsonResponse = scanner.useDelimiter("\\A").hasNext() ? scanner.next() : "";
-//                            scanner.close();
-//                        }
-//                        System.out.println("jsonResponse:\n" + jsonResponse);
-//
-//                    } catch (Throwable t) {
-//                        t.printStackTrace();
-//                    }
-//                }
-//            }
-//        });
-//    }
+    private void sendNotification(final String message, final String senderMail)
+    {
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                int SDK_INT = android.os.Build.VERSION.SDK_INT;
+                if (SDK_INT > 8) {
+                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                            .permitAll().build();
+                    StrictMode.setThreadPolicy(policy);
+                    String send_email;
+
+                    //This is a Simple Logic to Send Notification different Device Programmatically....
+                    String LoggedIn_User_Email =FirebaseAuth.getInstance().getCurrentUser().getEmail();
+                    OneSignal.sendTag("User_ID",FirebaseAuth.getInstance().getCurrentUser().getEmail());//sender email
+                    send_email=senderMail;//reciever email
+                    /*
+                    if (MainActivity.LoggedIn_User_Email.equals("user1@gmail.com")) {
+                        send_email = "user2@gmail.com";
+                    } else {
+                        send_email = "user1@gmail.com";
+                    }*/
+
+                    try {
+                        String jsonResponse;
+
+                        URL url = new URL("https://onesignal.com/api/v1/notifications");
+                        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                        con.setUseCaches(false);
+                        con.setDoOutput(true);
+                        con.setDoInput(true);
+
+                        con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                        con.setRequestProperty("Authorization", "Basic NTVjOWIxNmYtYjI0YS00NjU0LWE1YmEtYjM5YTM2OWQxZjIx");
+                        con.setRequestMethod("POST");
+
+                        String strJsonBody = "{"
+                                + "\"app_id\": \"c12cdbbf-7bd8-4c4f-b5ba-df37e6cc2d36\","
+
+                                + "\"filters\": [{\"field\": \"tag\", \"key\": \"User_ID\", \"relation\": \"=\", \"value\": \"" + send_email + "\"}],"
+
+                                + "\"data\": {\"foo\": \"bar\"},"
+                                + "\"contents\": {\"en\": \""+message+"\"}"
+                                + "}";
+
+
+                        System.out.println("strJsonBody:\n" + strJsonBody);
+
+                        byte[] sendBytes = strJsonBody.getBytes("UTF-8");
+                        con.setFixedLengthStreamingMode(sendBytes.length);
+
+                        OutputStream outputStream = con.getOutputStream();
+                        outputStream.write(sendBytes);
+
+                        int httpResponse = con.getResponseCode();
+                        System.out.println("httpResponse: " + httpResponse);
+
+                        if (httpResponse >= HttpURLConnection.HTTP_OK
+                                && httpResponse < HttpURLConnection.HTTP_BAD_REQUEST) {
+                            Scanner scanner = new Scanner(con.getInputStream(), "UTF-8");
+                            jsonResponse = scanner.useDelimiter("\\A").hasNext() ? scanner.next() : "";
+                            scanner.close();
+                        } else {
+                            Scanner scanner = new Scanner(con.getErrorStream(), "UTF-8");
+                            jsonResponse = scanner.useDelimiter("\\A").hasNext() ? scanner.next() : "";
+                            scanner.close();
+                        }
+                        System.out.println("jsonResponse:\n" + jsonResponse);
+
+                    } catch (Throwable t) {
+                        t.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
 }
